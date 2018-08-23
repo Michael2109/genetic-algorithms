8c00cf66ec72a3ccc3da17ed6b703bad8d09f863
Michael <michael.jhaywoods@gmail.com>
2018-08-23 14:02:01 +0100

    Added drawing of fish and food.

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,19 @@ pub struct App {
     gl: GlGraphics,
     // OpenGL drawing backend.
     rotation: f64,   // Rotation for the square.
+    fish: Vec<entity::Fish>,
+    food: Vec<entity::Food>
 }
 
 impl App {
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
+    }
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
@@ -32,21 +38,36 @@ impl App {
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            clear(BLACK, gl);
         });
+
+        for f in &self.fish {
+
+            self.gl.draw(args.viewport(), |c, gl| {
+
+                let transform = c.transform.trans(f.x.get(), f.y.get())
+                    .rot_rad(rotation)
+                    .trans(-25.0, -25.0);
+
+                // Draw a box rotating around the middle of the screen.
+                rectangle(RED, square, transform, gl);
+            });
+        }
+
+        for f in &self.food {
+
+            self.gl.draw(args.viewport(), |c, gl| {
+
+                let transform = c.transform.trans(f.x.get(), f.y.get())
+                    .rot_rad(rotation)
+                    .trans(-25.0, -25.0);
+
+                // Draw a box rotating around the middle of the screen.
+                rectangle(RED, square, transform, gl);
+            });
+        }
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
 }
 
 fn main() {
@@ -56,7 +77,7 @@ fn main() {
     // Create an Glutin window.
     let mut window: Window = WindowSettings::new(
         "spinning-square",
-        [200, 200],
+        [800, 600],
     )
         .opengl(opengl)
         .exit_on_esc(true)
@@ -67,9 +88,10 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
+        fish: Vec::new(),
+        food: Vec::new()
     };
 
-    entity::print_hello();
 
     let fish = entity::Fish {
         gl: GlGraphics::new(opengl),
@@ -91,11 +113,12 @@ fn main() {
 
         mass: 100.0,
 
-        x: Cell::new(50.0),
-        y: Cell::new(50.0)
+        x: Cell::new(500.0),
+        y: Cell::new(500.0),
     };
 
-    let mut vec: Vec<Box<entity::Entity>> = vec![Box::new(fish), Box::new(food)];
+    app.fish.push(fish);
+    app.food.push(food);
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
